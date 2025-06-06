ce1bd112a731d6e8ce59a7b885ddd2d442435645
zariusss <59838612+zariusss@users.noreply.github.com>
2025-06-06 07:41:38 +0200

    Limpieza del tema
    Limpieza del tema para evitar problemas de licencias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: DevFolio
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
